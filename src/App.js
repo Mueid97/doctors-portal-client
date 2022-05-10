f53eca7b83068a4424053dae5e5c628e0fb72c43
Mueid <mrmuyed579@gmail.com>
2022-05-10 20:03:06 +0600

    header and footer added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './pages/About/About';
 import Home from './pages/Home/Home';
+import Footer from './share/Footer/Footer';
+import Navbar from './share/Navbar/Navbar';
 
 function App() {
   return (
     <div >
+      <Navbar></Navbar>
        <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
       </Routes>
+      <Footer></Footer>
     </div>
   );
 }
